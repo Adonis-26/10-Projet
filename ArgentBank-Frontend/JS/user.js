1fab44a5761c1cbfb616dc0c9af2cb27ee78bce4
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-01-13 17:54:21 +0100

    installation du swagger npm install swagger-ui-express yamljs

--- ArgentBank-Frontend/JS/user.js
+++ ArgentBank-Frontend/JS/user.js
@@ -7,26 +7,29 @@ if (!token) {
 fetch('http://localhost:3001/api/v1/user/profile', {
   method: 'GET',
   headers: {
-    'Authorization': `Bearer ${token}`,
+    Authorization: `Bearer ${token}`,
     'Content-Type': 'application/json'
   }
 })
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => {
     if (!data.body) {
       throw new Error('Unauthorized')
     }
 
     const user = data.body
-
     document.getElementById('welcome-name').textContent =
       `${user.firstName} ${user.lastName}`
 
-    document.querySelector('.main-nav-item').innerHTML =
+    document.getElementById('nav-username').innerHTML =
       `<i class="fa fa-user-circle"></i> ${user.firstName}`
   })
-  .catch(error => {
-    console.error('Authentication error:', error)
+  .catch(() => {
     localStorage.removeItem('token')
     window.location.href = 'sign-in.html'
   })
+  
+document.getElementById('logout').addEventListener('click', () => {
+  localStorage.removeItem('token')
+  window.location.href = 'index.html'
+})
