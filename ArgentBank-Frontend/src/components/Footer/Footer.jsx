3d682d646edd495dee61e76727e71da836fbdae6
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-01-20 19:51:40 +0100

    reation du composant Footer

---
+++ ArgentBank-Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <div className="footer>
+       <footer ">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </div>
+  )
+}
+
+export default Footer
