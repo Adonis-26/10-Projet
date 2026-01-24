994c431a492b8e20a148383bdea2acdd04ad346c
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-01-25 00:22:55 +0100

    creation du composant welcomeback

---
+++ ArgentBank-Frontend/src/components/WebcomeBack/WebcomeBack.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function WebcomeBack() {
+  return (
+    <div class="header">
+        <h1>Welcome back<br /> Name !</h1>
+        <button class="edit-button">Edit Name</button>
+    </div>
+  )
+}
+
+export default WebcomeBack
