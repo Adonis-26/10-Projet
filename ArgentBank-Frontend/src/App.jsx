7998ac33bcf3b953b108b37544427f2119f7a3e1
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-01-26 19:47:25 +0100

    installation de sass et finition de configuration react. puis visualisation du composant header

--- ArgentBank-Frontend/src/App.jsx
+++ ArgentBank-Frontend/src/App.jsx
@@ -1,6 +1,12 @@
+import Header from "./components/Header/Header"
+
 function App() {
     return (
+        <>
+        <Header />
         <h1>React est installé 🚀</h1>
+        </>
+        
     )
 }
 
