b7c174e018b4a74344b812b2a1e30222e2b5d38a
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-01-28 18:14:57 +0100

    page accueil créée avec react

--- ArgentBank-Frontend/src/components/Footer/Footer.jsx
+++ ArgentBank-Frontend/src/components/Footer/Footer.jsx
@@ -3,8 +3,7 @@ import './Footer.scss'
 
 function Footer() {
   return (
-    <div className="footer>
-       <footer ">
+    <div className="footer">
       <p className="footer-text">Copyright 2020 Argent Bank</p>
     </div>
   )
