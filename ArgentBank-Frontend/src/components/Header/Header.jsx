d066461d0df25c3da71cb8485d57560eaf59aaea
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-02-02 19:23:38 +0100

    test des routes avec la page /sing

--- ArgentBank-Frontend/src/components/Header/Header.jsx
+++ ArgentBank-Frontend/src/components/Header/Header.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import './Header.scss'
 
 function Header() {
   return (
        <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link className="main-nav-logo" to="./">
         <img
           className="main-nav-logo-image"
-          src="./img/argentBankLogo.png"
+          src="/img/argentBankLogo.png"
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="./sign.html">
+        <Link className="main-nav-item" to="./sign">
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   )
