111a7253625ee9b1a1a446cf2864686d8a5366d0
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-02-05 18:41:29 +0100

    page sign terminée

--- ArgentBank-Frontend/src/components/Layout/Layout.jsx
+++ ArgentBank-Frontend/src/components/Layout/Layout.jsx
@@ -5,12 +5,13 @@ import { Outlet } from "react-router-dom"
 
 function Layout() {
   return (
-     <>
+    <div className="layout">
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   )
 }
+
 
 export default Layout
--- ArgentBank-Frontend/src/pages/Sign.jsx
+++ ArgentBank-Frontend/src/pages/Sign.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import './Sign.scss'
 
 function Sign() {
   return (
-    <div>
-      
-    </div>
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label for="username">Username</label>
+              <input type="text" id="email" required />
+            </div>
+            <div className="input-wrapper">
+              <label for="password">Password</label>
+              <input type="password" id="password" required />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+            </div>
+            <button type="submit" className="sign-in-button">Sign In</button>
+          </form>
+        </section>
+      </main>
+
   )
 }
 
